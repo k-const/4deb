4d4b9d3e2f17e16bc3018d331a5c4a7a894b1b65
k-const <78409869+k-const@users.noreply.github.com>
2021-03-23 18:41:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,19 @@ echo 'APT::Get::Install-Suggests "false";' >> /etc/apt/apt.conf
 echo 'deb http://deb.debian.org/debian/ stable main contrib non-free' > /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian/ stable-updates main contrib non-free' >> /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian-security stable/updates main' >> /etc/apt/sources.list
-apt update && apt -y upgrade && apt -y install apt-transport-https curl dirmngr wget
+apt update && apt -y upgrade && apt -y install apt-transport-https curl dirmngr flatpak wget
 
 # installing desktop environment and some necessary apps
-apt -y install at-spi2-core chrome-gnome-shell clementine cutecom doublecmd-gtk doublecmd-plugins \
-file-roller filezilla firefox-esr firefox-esr-l10n-ru fonts-ibm-plex fonts-noto-cjk-extra gdm3 \
-gnome-screenshot gnome-session gnome-session-canberra gnome-sushi gnome-terminal gnome-tweaks \
-gnome-user-docs gstreamer1.0-gtk3 htop libreoffice-calc libglx-mesa0 libreoffice-gnome \
-libreoffice-gtk3 libreoffice-impress libreoffice-l10n-ru libreoffice-writer macchanger \
-mactelnet-client mpv mtr nautilus-admin nautilus-extension-gnome-terminal nautilus-sendto \
-nautilus-share nautilus-wipe network-manager-l2tp-gnome network-manager-openconnect-gnome \
-network-manager-openvpn-gnome network-manager-pptp-gnome network-manager-ssh-gnome \
-network-manager-strongswan pavucontrol pwgen qbittorrent qpdfview qpdfview-djvu-plugin \
+apt -y install at-spi2-core audacity chrome-gnome-shell clementine cups-filters-core-drivers \
+cutecom doublecmd-gtk doublecmd-plugins file-roller filezilla firefox-esr firefox-esr-l10n-ru \
+fonts-ibm-plex fonts-noto-cjk-extra gdm3 gnome-screenshot gnome-session gnome-session-canberra \
+gnome-sushi gnome-terminal gnome-tweaks gnome-user-docs gstreamer1.0-gtk3 htop iptables-persistent \
+ladspa-plugin libreoffice-calc libglx-mesa0 libreoffice-gnome libreoffice-gtk3 libreoffice-impress \
+libreoffice-l10n-ru libreoffice-writer macchanger mactelnet-client mpv mtr nautilus-admin \
+nautilus-extension-gnome-terminal nautilus-sendto nautilus-share nautilus-wipe \
+network-manager-l2tp-gnome network-manager-openconnect-gnome network-manager-openvpn-gnome \
+network-manager-pptp-gnome network-manager-ssh-gnome network-manager-strongswan pavucontrol \
+printer-driver-all printer-driver-cups-pdf pwgen qbittorrent qpdfview qpdfview-djvu-plugin \
 qpdfview-ps-plugin qt5-style-plugins rar sipcalc tcpdump thunderbird thunderbird-l10n-ru \
-ttf-mscorefonts-installer ttf-unifont uance unrar unzip vim-gtk3 wireshark-gtk xdg-utils \
+ttf-mscorefonts-installer ttf-unifont unace unrar unzip vim-gtk3 wireshark-gtk xdg-utils \
 xserver-xorg xserver-xorg-input-libinput xserver-xorg-video-ati xserver-xorg-video-vesa zip

9aa524a9d3e8c486632b400b6286503c66a9c886
k-const <78409869+k-const@users.noreply.github.com>
2021-03-23 19:02:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,13 +14,12 @@ cutecom doublecmd-gtk doublecmd-plugins file-roller filezilla firefox-esr firefo
 fonts-ibm-plex fonts-noto-cjk-extra gdm3 gimp gimp-data-extras gimp-dds gimp-gap gimp-gmic \
 gimp-gutenprint gimp-lensfun gimp-plugin-registry gimp-ufraw gtkam-gimp gnome-session \
 gnome-session-canberra gnome-sushi gnome-terminal gnome-tweaks gnome-user-docs gstreamer1.0-gtk3 \
-htop iptables-persistent ladspa-plugin libreoffice-calc libglx-mesa0 libreoffice-gnome \
-libreoffice-gtk3 libreoffice-impress libreoffice-l10n-ru libreoffice-writer macchanger \
-mactelnet-client mpv mtr nautilus-admin nautilus-extension-gnome-terminal nautilus-sendto \
-nautilus-share nautilus-wipe network-manager-l2tp-gnome network-manager-openconnect-gnome \
-network-manager-openvpn-gnome network-manager-pptp-gnome network-manager-ssh-gnome \
-network-manager-strongswan pavucontrol printer-driver-all printer-driver-cups-pdf pwgen qbittorrent \
-qpdfview qpdfview-djvu-plugin qpdfview-ps-plugin qt5-style-plugins rar screengrab sipcalc tcpdump \
-thunderbird thunderbird-l10n-ru ttf-mscorefonts-installer ttf-unifont unace unrar unzip vim-gtk3 \
-wireshark-gtk xdg-utils xserver-xorg xserver-xorg-input-libinput xserver-xorg-video-ati \
-xserver-xorg-video-vesa zip
+htop iptables-persistent libreoffice-calc libglx-mesa0 libreoffice-gnome libreoffice-gtk3 \
+libreoffice-impress libreoffice-l10n-ru libreoffice-writer macchanger mactelnet-client mpv mtr \
+nautilus-admin nautilus-extension-gnome-terminal nautilus-sendto nautilus-share nautilus-wipe \
+network-manager-l2tp-gnome network-manager-openconnect-gnome network-manager-openvpn-gnome \
+network-manager-pptp-gnome network-manager-ssh-gnome network-manager-strongswan pavucontrol \
+printer-driver-all printer-driver-cups-pdf pwgen qbittorrent qpdfview qpdfview-djvu-plugin \
+qpdfview-ps-plugin qt5-style-plugins rar screengrab sipcalc speedcrunch tcpdump thunderbird \
+thunderbird-l10n-ru ttf-mscorefonts-installer ttf-unifont unace unrar unzip vim-gtk3 wireshark-gtk \
+xdg-utils xserver-xorg xserver-xorg-input-libinput xserver-xorg-video-ati xserver-xorg-video-vesa zip

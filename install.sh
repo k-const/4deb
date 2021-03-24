9a71dfc890ef49819b62586e3fd96c24e3723256
k-const <78409869+k-const@users.noreply.github.com>
2021-03-24 11:33:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,17 +15,18 @@ firefox-esr-l10n-ru flatpak fonts-ibm-plex fonts-noto-cjk-extra gdm3 gimp gimp-d
 gimp-gap gimp-gmic gimp-gutenprint gimp-lensfun gimp-plugin-registry gimp-ufraw gnome-bluetooth \
 gnome-control-center gnome-online-accounts gnome-screenshot gnome-session gnome-session-canberra \
 gnome-sushi gnome-terminal gnome-tweaks gnome-user-docs gstreamer1.0-pulseaudio gtkam-gimp \
-gvfs-backends htop iptables-persistent libcanberra-pulse libglx-mesa0 libreoffice-calc \
-libreoffice-evolution libreoffice-gnome libreoffice-gtk3 libreoffice-impress libreoffice-l10n-ru \
-libreoffice-writer macchanger mactelnet-client mesa-vdpau-drivers modemmanager mpv mtr mythes-en-us \
-mythes-ru nautilus-admin nautilus-extension-gnome-terminal nautilus-sendto nautilus-share \
-nautilus-wipe network-manager-l2tp-gnome network-manager-openconnect-gnome \
-network-manager-openvpn-gnome network-manager-pptp-gnome network-manager-ssh-gnome pavucontrol \
-pidgin pidgin-audacious pidgin-blinklight pidgin-otr pidgin-plugin-pack printer-driver-all \
-printer-driver-cups-pdf pulseaudio pulseaudio-module-bluetooth pulseaudio-module-gsettings pwgen \
-qbittorrent qpdfview \qpdfview-djvu-plugin qpdfview-ps-plugin qt5-style-plugins rar rtkit sipcalc \
-speedcrunch tcpdump tftpd ttf-mscorefonts-installer ttf-unifont unace unrar unzip usb-modeswitch \
-vim-gtk3 vinagre wireshark-gtk xdg-utils xserver-xephyr xserver-xorg xserver-xorg-input-libinput \
+gvfs-backends htop hunspell-en-us hunspell-ru hyphen-en-us hyphen-ru iptables-persistent \
+libcanberra-pulse libglx-mesa0 libreoffice-calc libreoffice-evolution libreoffice-gnome \
+libreoffice-gtk3 libreoffice-impress libreoffice-l10n-ru libreoffice-writer macchanger \
+mactelnet-client mesa-vdpau-drivers modemmanager mpv mtr mythes-en-us mythes-ru nautilus-admin \
+nautilus-extension-gnome-terminal nautilus-sendto nautilus-share nautilus-wipe \
+network-manager-l2tp-gnome network-manager-openconnect-gnome network-manager-openvpn-gnome \
+network-manager-pptp-gnome network-manager-ssh-gnome pavucontrol pidgin pidgin-audacious \
+pidgin-blinklight pidgin-otr pidgin-plugin-pack printer-driver-all printer-driver-cups-pdf \
+pulseaudio pulseaudio-module-bluetooth pulseaudio-module-gsettings pwgen qbittorrent qpdfview \
+qpdfview-djvu-plugin qpdfview-ps-plugin qt5-style-plugins rar rtkit sipcalc speedcrunch tcpdump \
+tftpd ttf-mscorefonts-installer ttf-unifont unace unrar unzip usb-modeswitch vim-gtk3 vinagre \
+wireshark-gtk xdg-utils xserver-xephyr xserver-xorg xserver-xorg-input-libinput \
 xserver-xorg-video-ati xserver-xorg-video-vesa zip
 
 systemctl disable tftpd

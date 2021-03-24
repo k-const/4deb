71b9bebda273934fa1ff6a2affcd7dfebc8f9d38
k-const <78409869+k-const@users.noreply.github.com>
2021-03-24 10:59:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,22 +9,24 @@ echo 'deb http://deb.debian.org/debian-security stable/updates main' >> /etc/apt
 apt update && apt -y upgrade && apt -y install apt-transport-https ca-certificates curl dirmngr wget
 
 # installing desktop environment and some necessary apps
-apt -y install at-spi2-core audacity chrome-gnome-shell clementine cups-filters-core-drivers cutecom \
-dbus-user-session dia dia-shapes doublecmd-gtk doublecmd-plugins file-roller filezilla firefox-esr \
-firefox-esr-l10n-ru flatpak fonts-ibm-plex fonts-noto-cjk-extra gdm3 gimp gimp-data-extras gimp-dds \
-gimp-gap gimp-gmic gimp-gutenprint gimp-lensfun gimp-plugin-registry gimp-ufraw gnome-bluetooth \
-gnome-control-center gnome-online-accounts gnome-screenshot gnome-session gnome-session-canberra \
-gnome-sushi gnome-terminal gnome-tweaks gnome-user-docs gstreamer1.0-pulseaudio gtkam-gimp htop \
-iptables-persistent libcanberra-pulse libglx-mesa0 libreoffice-calc libreoffice-gnome \
-libreoffice-gtk3 libreoffice-impress libreoffice-l10n-ru libreoffice-writer macchanger \
-mactelnet-client mesa-vdpau-drivers mpv mtr nautilus-admin nautilus-extension-gnome-terminal \
+apt -y install at-spi2-core audacious audacity chrome-gnome-shell cups-filters-core-drivers cutecom \
+dbus-user-session dia dia-shapes doublecmd-gtk doublecmd-plugins evolution evolution-plugins \
+file-roller filezilla firefox-esr firefox-esr-l10n-ru flatpak fonts-ibm-plex fonts-noto-cjk-extra \
+gdm3 gimp gimp-data-extras gimp-dds gimp-gap gimp-gmic gimp-gutenprint gimp-lensfun \
+gimp-plugin-registry gimp-ufraw gnome-bluetooth gnome-control-center gnome-online-accounts \
+gnome-screenshot gnome-session gnome-session-canberra gnome-sushi gnome-terminal gnome-tweaks \
+gnome-user-docs gstreamer1.0-pulseaudio gtkam-gimp htop iptables-persistent libcanberra-pulse \
+libglx-mesa0 libreoffice-calc libreoffice-evolution libreoffice-gnome libreoffice-gtk3 \
+libreoffice-impress libreoffice-l10n-ru libreoffice-writer macchanger mactelnet-client \
+mesa-vdpau-drivers modemmanager mpv mtr nautilus-admin nautilus-extension-gnome-terminal \
 nautilus-sendto nautilus-share nautilus-wipe network-manager-l2tp-gnome \
 network-manager-openconnect-gnome network-manager-openvpn-gnome network-manager-pptp-gnome \
-network-manager-ssh-gnome pavucontrol printer-driver-all printer-driver-cups-pdf pulseaudio \
+network-manager-ssh-gnome pavucontrol pidgin pidgin-audacious pidgin-blinklight pidgin-otr \
+pidgin-plugin-pack printer-driver-all printer-driver-cups-pdf pulseaudio \
 pulseaudio-module-bluetooth pulseaudio-module-gsettings pwgen qbittorrent qpdfview \
 qpdfview-djvu-plugin qpdfview-ps-plugin qt5-style-plugins rar rtkit sipcalc speedcrunch tcpdump \
-thunderbird thunderbird-l10n-ru ttf-mscorefonts-installer ttf-unifont unace unrar unzip vim-gtk3 \
-wireshark-gtk xdg-utils xserver-xorg xserver-xorg-input-libinput xserver-xorg-video-ati \
-xserver-xorg-video-vesa zip
+ttf-mscorefonts-installer ttf-unifont unace unrar unzip usb-modeswitch vim-gtk3 wireshark-gtk \
+xdg-utils xserver-xorg xserver-xorg-input-libinput xserver-xorg-video-ati xserver-xorg-video-vesa zip
 
+# cleaning apt packages cache
 apt clean

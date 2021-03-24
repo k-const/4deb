81d7b8004e59f3e3e973671df5155f3c80ba08d2
k-const <78409869+k-const@users.noreply.github.com>
2021-03-24 11:28:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,23 +10,25 @@ apt update && apt -y upgrade && apt -y install apt-transport-https ca-certificat
 
 # installing desktop environment and some necessary apps
 apt -y install at-spi2-core audacious audacity chrome-gnome-shell cups-filters-core-drivers cutecom \
-dbus-user-session dia dia-shapes doublecmd-gtk doublecmd-plugins evolution evolution-plugins \
-file-roller filezilla firefox-esr firefox-esr-l10n-ru flatpak fonts-ibm-plex fonts-noto-cjk-extra \
-gdm3 gimp gimp-data-extras gimp-dds gimp-gap gimp-gmic gimp-gutenprint gimp-lensfun \
-gimp-plugin-registry gimp-ufraw gnome-bluetooth gnome-control-center gnome-online-accounts \
-gnome-screenshot gnome-session gnome-session-canberra gnome-sushi gnome-terminal gnome-tweaks \
-gnome-user-docs gstreamer1.0-pulseaudio gtkam-gimp htop iptables-persistent libcanberra-pulse \
-libglx-mesa0 libreoffice-calc libreoffice-evolution libreoffice-gnome libreoffice-gtk3 \
-libreoffice-impress libreoffice-l10n-ru libreoffice-writer macchanger mactelnet-client \
-mesa-vdpau-drivers modemmanager mpv mtr nautilus-admin nautilus-extension-gnome-terminal \
-nautilus-sendto nautilus-share nautilus-wipe network-manager-l2tp-gnome \
-network-manager-openconnect-gnome network-manager-openvpn-gnome network-manager-pptp-gnome \
-network-manager-ssh-gnome pavucontrol pidgin pidgin-audacious pidgin-blinklight pidgin-otr \
-pidgin-plugin-pack printer-driver-all printer-driver-cups-pdf pulseaudio \
-pulseaudio-module-bluetooth pulseaudio-module-gsettings pwgen qbittorrent qpdfview \
-qpdfview-djvu-plugin qpdfview-ps-plugin qt5-style-plugins rar rtkit sipcalc speedcrunch tcpdump \
-ttf-mscorefonts-installer ttf-unifont unace unrar unzip usb-modeswitch vim-gtk3 wireshark-gtk \
-xdg-utils xserver-xorg xserver-xorg-input-libinput xserver-xorg-video-ati xserver-xorg-video-vesa zip
+dbus-user-session dia dia-shapes evolution evolution-plugins file-roller filezilla firefox-esr \
+firefox-esr-l10n-ru flatpak fonts-ibm-plex fonts-noto-cjk-extra gdm3 gimp gimp-data-extras gimp-dds \
+gimp-gap gimp-gmic gimp-gutenprint gimp-lensfun gimp-plugin-registry gimp-ufraw gnome-bluetooth \
+gnome-control-center gnome-online-accounts gnome-screenshot gnome-session gnome-session-canberra \
+gnome-sushi gnome-terminal gnome-tweaks gnome-user-docs gstreamer1.0-pulseaudio gtkam-gimp \
+gvfs-backends htop iptables-persistent libcanberra-pulse libglx-mesa0 libreoffice-calc \
+libreoffice-evolution libreoffice-gnome libreoffice-gtk3 libreoffice-impress libreoffice-l10n-ru \
+libreoffice-writer macchanger mactelnet-client mesa-vdpau-drivers modemmanager mpv mtr mythes-en-us \
+mythes-ru nautilus-admin nautilus-extension-gnome-terminal nautilus-sendto nautilus-share \
+nautilus-wipe network-manager-l2tp-gnome network-manager-openconnect-gnome \
+network-manager-openvpn-gnome network-manager-pptp-gnome network-manager-ssh-gnome pavucontrol \
+pidgin pidgin-audacious pidgin-blinklight pidgin-otr pidgin-plugin-pack printer-driver-all \
+printer-driver-cups-pdf pulseaudio pulseaudio-module-bluetooth pulseaudio-module-gsettings pwgen \
+qbittorrent qpdfview \qpdfview-djvu-plugin qpdfview-ps-plugin qt5-style-plugins rar rtkit sipcalc \
+speedcrunch tcpdump tftpd ttf-mscorefonts-installer ttf-unifont unace unrar unzip usb-modeswitch \
+vim-gtk3 vinagre wireshark-gtk xdg-utils xserver-xephyr xserver-xorg xserver-xorg-input-libinput \
+xserver-xorg-video-ati xserver-xorg-video-vesa zip
+
+systemctl disable tftpd
 
 # cleaning apt packages cache
 apt clean
